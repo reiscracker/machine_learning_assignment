c1cbbd330d9a4cd2fe36b2af084bb4693fcd901a
Simon A <simonh.arnold@gmail.com>
2015-06-26 16:10:02 +0200

    Added some statistical data about users

--- users_explorative.py
+++ users_explorative.py
@@ -4,5 +4,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Read training and user data
-trackFrame = pd.read_csv('train.csv')
+#trackFrame = pd.read_csv('train.csv')
 userFrame = pd.read_csv('users.csv')
+
+# Print overall statistics about users
+userCount = userFrame.RESPID.count()
+maleUserCount = userFrame[userFrame.GENDER == 'Male']['RESPID'].count()
+femaleUserCount = userFrame[userFrame.GENDER == 'Female']['RESPID'].count()
+print("Count of distinct users: %s" % userCount)
+print("Count of male users: %s" % maleUserCount)
+print("Count of female users: %s" % femaleUserCount)
+print("(%f percent male, %f percent female)" % ( float(maleUserCount) / userCount * 100, float(femaleUserCount) / userCount * 100 ))
