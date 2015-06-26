604a597654c8c6419712fe5d8cf2a4e2e05fdc29
Lukas Hodel <l.hodel@de.edenspiekermann.com>
2015-06-26 16:20:04 +0200

    Mark inconsistent records

--- users_explorative.py
+++ users_explorative.py
@@ -6,3 +6,12 @@ import matplotlib.pyplot as plt
 # Read training and user data
 trackFrame = pd.read_csv('train.csv')
 userFrame = pd.read_csv('users.csv')
+
+# Mark inconsistent data
+unique_track_users = np.unique(trackFrame.User)
+unique_users = np.unique(userFrame.RESPID)
+udiff = np.setdiff1d(unique_track_users, unique_users).size
+trackFrame['inconsistent'] = \
+    trackFrame.User.apply(lambda uid: uid in udiff)
+userFrame['inconsistent'] = \
+    userFrame.RESPID.apply(lambda uid: uid in udiff)
