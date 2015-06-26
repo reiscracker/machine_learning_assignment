aec460ef980365bbe0eb8608c2f2c40b46b2e233
Lukas Hodel <l.hodel@de.edenspiekermann.com>
2015-06-26 14:52:38 +0200

    Adds counting of Artist/User/Track

--- regions.sh
+++ regions.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo "Possible Genders:"
+echo -e "\nPossible Genders:"
 cat users.csv | awk -F ',' '{print $2}' | sort | uniq        
-echo "Working status:"
+echo -e "\nAge:"
 cat users.csv | awk -F ',' '{print $3}' | sort | uniq        
-echo "Regions:"
+echo -e "\nWorking Status:"
 cat users.csv | awk -F ',' '{print $4}' | sort | uniq        
+echo -e "\nRegions:"
+cat users.csv | awk -F ',' '{print $5}' | sort | uniq        
 
