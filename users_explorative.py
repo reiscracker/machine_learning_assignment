cacb671d9ce21d3d53e2b896544d95cbc82cf5ef
Simon A <simonh.arnold@gmail.com>
2015-06-26 17:32:13 +0200

    Distribution of age plotted

--- users_explorative.py
+++ users_explorative.py
@@ -15,3 +15,16 @@ print("Count of distinct users: %s" % userCount)
 print("Count of male users: %s" % maleUserCount)
 print("Count of female users: %s" % femaleUserCount)
 print("(%f percent male, %f percent female)" % ( float(maleUserCount) / userCount * 100, float(femaleUserCount) / userCount * 100 ))
+print("Youngest user: %s, oldest user %s" % (userFrame['AGE'].min(), userFrame['AGE'].max()))
+
+# Distribution of age
+plt.figure(figsize=(20,8))
+plt.subplot(1, 2, 1)
+userFrame['AGE'].hist(bins=20)
+plt.title("Distribution of age")
+plt.xlabel("Age"); plt.ylabel("Amount of users with that age")
+
+plt.subplot(1, 2, 2)
+userFrame.boxplot(column="AGE")
+plt.show()
+
